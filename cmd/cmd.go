41024ff5948d08d954672a7f9ca8901ec2afae0c
AAAwlx <561639418@qq.com>
2024-11-01 14:57:32 +0800

    libbpfgo

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -15,26 +15,25 @@ type CommandRequest struct {
 func Execute(c *gin.Context) {
 	var cmdRequest CommandRequest
 	
-    // 解析请求的 JSON 数据
-    if err := c.ShouldBindJSON(&cmdRequest); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
+	// 解析请求的 JSON 数据
+	if err := c.ShouldBindJSON(&cmdRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
     }
 	if cmdRequest.Buffermod == "skb" {//skb包
-        
 		c.JSON(http.StatusOK, gin.H{"result": "skb get"})
-        return
+		return
 	}else if cmdRequest.Buffermod == "raw"{//raw包
 		c.JSON(http.StatusOK, gin.H{"result": "raw get"})
-        return
+		return
 	}else if cmdRequest.Buffermod == "mbuf" {//mbuf包
 		c.JSON(http.StatusOK, gin.H{"result": "mbuf get"})
-        return
+		return
 	}else{
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "参数不合规"})
-        return
-    }
-    // 返回结果
-    //c.JSON(http.StatusOK, gin.H{"result": "succed"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "参数不合规"})	
+		return
+	}
+	// 返回结果
+	//c.JSON(http.StatusOK, gin.H{"result": "succed"})
 }
 
