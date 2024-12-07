b20a5f30330ac0f8bc226b0cfae7609080b37eec
AAAwlx <561639418@qq.com>
2024-12-07 22:12:23 +0800

    抓包

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,12 +1,14 @@
 package cmd
 
 import(
-	//"fmt"
-	"context"
-	"log"
+	"fmt"
+	//"context"
+	//"log"
+	"os"      // 导入 os 包以便进行文件操作
+    "os/exec" // 导入 exec 包以执行外部命令
     "net/http"
 	"github.com/gin-gonic/gin"
-	"skb/dump"
+	//"skb/dump"
 )
 
 // 请求参数结构体
@@ -46,11 +48,11 @@ func Execute(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"result": "skb get"})
 		return
 	}else if cmdRequest.Buffermod == "raw"{//raw包
-		doRawDumap(&cmdRequest)
+		//doRawDumap(&cmdRequest)
 		c.JSON(http.StatusOK, gin.H{"result": "raw get"})
 		return
 	}else if cmdRequest.Buffermod == "mbuf" {//mbuf包
-		doMbufDump(&cmdRequest)
+		//doMbufDump(&cmdRequest)
 		c.JSON(http.StatusOK, gin.H{"result": "mbuf get"})
 		return
 	}else{
@@ -61,56 +63,65 @@ func Execute(c *gin.Context) {
 	//c.JSON(http.StatusOK, gin.H{"result": "succed"})
 }
 
-func commOption(c *CommandRequest) *dump.Option {
-	opt := &dump.Option{
-		UserFilterFilePath:         c.UserFilter,//用户自定义 filter （过滤器）文件所在的路径
-		UserActionFilePath:         c.UserAction,//用户自定义的 Action 文件所在的路径
-		TcpdumpFlags:               c.TcpdumpOpts,//设置tcpdump的参数
-		TcpdumpExpression:          c.Expression,//数据包表达式
-		TraceFunction:              c.Filter,//挂载方法与挂载函数名
-		DumpWriteFilePath:          c.WriteFile,//转储写入文件的路径
-		DumpWriteFileRotate:        c.WriteFileRotate,//文件尺寸，超过后覆盖写
-		DumpCount:                  c.CaptureCount,//抓到包的数量，超过该数量后的包将不再抓取
-		CaptureMaxSize:             c.CaptureMaxSize,//这个size是指抓的包，保存到buff的大小，如果包长超过这个值，则包会被截断
-		IsDryRun:                   c.DryRun,
-	}
-
-	return opt
-}
-
-// runDump 函数执行给定的 dump.Operator 操作
-func runDump(dumpOp dump.Operator) {
-    // 调用 dumpOperator 的 Run 方法，使用一个背景上下文
-    err := dumpOp.Run(context.TODO())
-    if err != nil {
-        // 如果发生错误，则记录错误并终止程序
-        log.Fatalf("Dump operator run err: %v", err)
-    }
-}
+//func commOption(c *CommandRequest) *dump.Option {
+//	opt := &dump.Option{
+//		UserFilterFilePath:         c.UserFilter,//用户自定义 filter （过滤器）文件所在的路径
+//		UserActionFilePath:         c.UserAction,//用户自定义的 Action 文件所在的路径
+//		TcpdumpFlags:               c.TcpdumpOpts,//设置tcpdump的参数
+//		TcpdumpExpression:          c.Expression,//数据包表达式
+//		TraceFunction:              c.Filter,//挂载方法与挂载函数名
+//		DumpWriteFilePath:          c.WriteFile,//转储写入文件的路径
+//		DumpWriteFileRotate:        c.WriteFileRotate,//文件尺寸，超过后覆盖写
+//		DumpCount:                  c.CaptureCount,//抓到包的数量，超过该数量后的包将不再抓取
+//		CaptureMaxSize:             c.CaptureMaxSize,//这个size是指抓的包，保存到buff的大小，如果包长超过这个值，则包会被截断
+//		IsDryRun:                   c.DryRun,
+//	}
+//
+//	return opt
+//}
+//
+//// runDump 函数执行给定的 dump.Operator 操作
+//func runDump(dumpOp dump.Operator) {
+//    // 调用 dumpOperator 的 Run 方法，使用一个背景上下文
+//    err := dumpOp.Run(context.TODO())
+//    if err != nil {
+//        // 如果发生错误，则记录错误并终止程序
+//        log.Fatalf("Dump operator run err: %v", err)
+//    }
+//}
 
 func doSkbDumap(c *CommandRequest){
-	opt := &dump.SkbOption{
-		Option:         *commOption(c),
-		Interface:      c.Interface,
-		IsDumpStack:    c.IsDumpStack,
-		IsFakeHdr:      c.IsSkbFakeHdr,
-		IsUseSkbData:   c.IsUseSkbData,
-		SkbDataOffset:  c.UseSkbDataOffset,
-	}
+	cmdPath := "/home/ylx/bpf-developer-tutorial/src/41-xdp-tcpdump/xdp-tcpdump"
+    
+    // 打开文件进行写入（如果文件不存在则创建）
+    outputFile, err := os.Create("/home/ylx/我的项目/skb/skb.txt")
+    if err != nil {
+        fmt.Println("Error creating output file:", err)
+        return
+    }
+    defer outputFile.Close() // 确保程序退出时关闭文件
 
-	skbDump, err := dump.NewSkbDump(opt)
-	if err != nil {
-		log.Fatalf("Dump err: %v", err)
-		return
-	}
+    // 创建一个 exec.Command 对象，使用 sudo 运行命令
+    cmd := exec.Command("sudo", cmdPath, "wlan0") // 使用 sudo 来运行需要特权的命令
 
-	runDump(skbDump)
+    // 设置命令的输出重定向到文件
+    cmd.Stdout = outputFile  // 标准输出重定向到文件
+    cmd.Stderr = outputFile  // 标准错误也重定向到文件
+
+    // 运行命令
+    err = cmd.Run()
+    if err != nil {
+        fmt.Println("Error executing command:", err)
+        return
+    }
+
+    fmt.Println("Command output has been written to /home/ylx/我的项目/skb/skb.txt")
 }
 
-func doRawDumap(c *CommandRequest){
+//func doRawDumap(c *CommandRequest){
 
-}
+//}
 
-func doMbufDump(c *CommandRequest){
+//func doMbufDump(c *CommandRequest){
 	
-}
+//}
--- dump/dump.go
+++ dump/dump.go
@@ -1,33 +1,34 @@
 package dump
 
-import(
-	"context"
-	"skb/dump/driver"
-)
-
-type Operator interface {
-	Run(ctx context.Context) error
-}
-
-//通用选项参数的值
-type Option struct {
-	UserFilterFilePath  string
-	UserActionFilePath  string
-	UserOutputColor     string
-	TcpdumpFlags        string
-	TcpdumpExpression   string
-	TraceFunction       string
-	DumpWriteFilePath   string
-	DumpWriteFileRotate uint32
-
-	DumpCount      uint32
-	CaptureMaxSize uint32
-
-	IsDryRun                   bool
-}
-
-// dumpBaseImpl 是一个结构体，包含了与 dump 操作相关的基础实现
-type dumpBaseImpl struct {
-    driver driver.Operator      // 驱动程序操作接口，用于与底层驱动交互
-    //parser tparser.Operator     // 解析器操作接口，用于解析数据包或其他输入数据
-}
+//import(
+//	"context"
+//	"skb/dump/driver"
+//)
+//
+//type Operator interface {
+//	Run(ctx context.Context) error
+//}
+//
+////通用选项参数的值
+//type Option struct {
+//	UserFilterFilePath  string
+//	UserActionFilePath  string
+//	UserOutputColor     string
+//	TcpdumpFlags        string
+//	TcpdumpExpression   string
+//	TraceFunction       string
+//	DumpWriteFilePath   string
+//	DumpWriteFileRotate uint32
+//
+//	DumpCount      uint32
+//	CaptureMaxSize uint32
+//
+//	IsDryRun                   bool
+//}
+//
+//// dumpBaseImpl 是一个结构体，包含了与 dump 操作相关的基础实现
+//type dumpBaseImpl struct {
+//    driver driver.Operator      // 驱动程序操作接口，用于与底层驱动交互
+//    //parser tparser.Operator     // 解析器操作接口，用于解析数据包或其他输入数据
+//}
+//
